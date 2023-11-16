d8485ddc828edd0eb0ef692bdf5e930bcf7c424b
Elvis <kriselijahk1@gmail.com>
2023-11-16 12:22:55 -0800

    updated server port

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const api_routes = require('./routes/api-routes');
 const html_routes = require('./routes/html-routes');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const path = require('path');
 
 const app = express();
